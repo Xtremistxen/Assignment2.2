f8b4701eefa024cb6264af0f567d4cf4f8ab90fb
Xtremistxen <131495799+Xtremistxen@users.noreply.github.com>
2025-03-06 18:16:27 -0600

    Reworked RPSLOTR
    Added a helper function to clean up Images for up to 4 players since using a If/Else statement was repetitive coding, and added another Image Ring.jpg(player who has ring will always loose)

--- functionRPSLOTR.js
+++ functionRPSLOTR.js
@@ -1,66 +1,106 @@
 function RPS()
 {
-    var player1 = Math.ceil(Math.random() *3)
-    // This will generate a random number 1-3
-    var player2 = Math.ceil(Math.random() *3)
+    var player1 = Math.ceil(Math.random() *4)
+    // This will generate a random number 1-4
+    var player2 = Math.ceil(Math.random() *4)
+    var player3 = Math.ceil(Math.random() *4)
+    var player4 = Math.ceil(Math.random() *4)
 
     var rockImg = "Rock.jpg";
     var paperImg = "Paper.jpg";
     var scissorsImg = "Scissors.jpg";
+    var ringImg = "Ring.jpg"; // Added the ring the ultimate weapon looses over everything
 
-    // This will show the img at random for both players
-    if (player1 == 1)
-        {
-          document.getElementById("Player1img").src = rockImg;
-        }
+    
+    // This Helper function is alot better than my previous repetitive code I had before allowing me to assignImage to the players much cleaner form of code
+    function assignImage(player, playerImgElementId) 
+    {
+      if (player == 1) 
+         {
+          document.getElementById(playerImgElementId).src = rockImg;
+         }  
+      else if (player == 2) 
+         {
+          document.getElementById(playerImgElementId).src = paperImg;
+         } 
+      else if (player == 3) 
+         {
+          document.getElementById(playerImgElementId).src = scissorsImg;
+         } 
+      else if (player == 4) 
+         {
+          document.getElementById(playerImgElementId).src = ringImg;
+         }
+  }
 
-           else if (player1 == 2)
-            
-             {
-                document.getElementById("Player1img").src = paperImg;
-             }
-            
-     
-            else
-            
-             {
-                document.getElementById("Player1img").src = scissorsImg;
-             }
-            
+  assignImage(player1, "Player1img");
+  assignImage(player2, "Player2img");
+  assignImage(player3, "Player3img");
+  assignImage(player4, "Player4img");
 
-            if (player2 == 1)
-            
-             {
-                document.getElementById("Player2img").src = rockImg;
-             }
-            
-            
-             else if (player2 == 2)
-             
-              {
-                document.getElementById("Player2img").src = paperImg;
-              }
-             
-            
-             else
-                
-              {
-                 document.getElementById("Player2img").src = scissorsImg;
-              }
-
-              // This will show Draw or Wins
-if (player1 == player2)
+function checkWinner(playerA, playerB) // This will check for a tie with same img
 {
-    document.getElementById("finalResult").innerHTML = "DRAW";
-}
+  // New rule that the ring looses to everything since it always gets away
+  if (playerA === 4)
+  {
+   return 2; // PlayerA otherwords will loose and playerB will win
+  }
+  else if (playerB === 4)
+  {
+    return 1; // Opposite of above
+  }
 
-else if ((player1 == 1 && player2 == 3) || (player1 == 2 && player2 == 1) || (player1 == 3 && player2 == 2))
-{
-    document.getElementById("finalResult").innerHTML = "Player 1 Wins";
-}
-else
-{
-    document.getElementById("finalResult").innerHTML = "Player 2 Wins";
-}
+  // This is the normal rock, paper scissors rules
+  if ((playerA === 1 && playerB === 3) 
+  || (playerA === 2 && playerB === 1) 
+  || (playerA === 3 && playerB === 2))
+  {
+   return 1;
+  }
 
+  return 2;
+}
+ // This will determine the winners
+var winners = [];
+
+
+  // Since we now have more than two players we needed a way to check which player wins between the 1-4 players
+  // This will check player 1 vs player 2 and so on with the list below
+  if (checkWinner(player1, player2) === 1) winners.push("Player 1");
+  else if (checkWinner(player1, player2) === 2) winners.push("Player 2");
+
+  if (checkWinner(player1, player3) === 1) winners.push("Player 1");
+  else if (checkWinner(player1, player3) === 2) winners.push("Player 3");
+
+  if (checkWinner(player1, player4) === 1) winners.push("Player 1");
+  else if (checkWinner(player1, player4) === 2) winners.push("Player 4");
+
+  if (checkWinner(player2, player3) === 1) winners.push("Player 2");
+  else if (checkWinner(player2, player3) === 2) winners.push("Player 3");
+
+  if (checkWinner(player2, player4) === 1) winners.push("Player 2");
+  else if (checkWinner(player2, player4) === 2) winners.push("Player 4");
+
+  if (checkWinner(player3, player4) === 1) winners.push("Player 3");
+  else if (checkWinner(player3, player4) === 2) winners.push("Player 4");
+
+   // Determine the result message
+   var resultMessage = "";
+
+   if (winners.length === 0) {
+       resultMessage = "It's a Draw!"; // No winners, all tied
+   } else 
+   {
+    if (winners.length === 1) 
+      {
+        resultMessage = winners[0] + " Wins!";
+      } 
+    else 
+      {
+         resultMessage = winners.join(", ") + " Win!";
+      }
+   }
+
+   // Display the final result
+   document.getElementById("finalResult").innerHTML = resultMessage;
 }
